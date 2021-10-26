ae7f8b9910fec17facb16e1d0c5598a524fb9399
Juan Domandl <jdomandl@Juans-MacBook-Pro.local>
2021-10-25 21:25:53 -0300

    pre dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import * as React from 'react';
 import './App.css';
+import ListarPartidas from './ListarPartidas.js'
 
-
+const PARTIDAS_URL = 'http://192.168.0.152:81/partidas';
 function App(){
   return(
-    <h1>Bienvenido a Misterio</h1>
+    <div>
+    <h1>Bienvenido a Misterio</h1> 
+    <ListarPartidas url={PARTIDAS_URL}/>
+    </div>
   );
 }
-
+ 
 export default App;
