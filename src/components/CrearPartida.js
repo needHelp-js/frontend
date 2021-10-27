6eca9e294dcf83d860a4871fdb75cf36e0f0e158
Matiata <matizmurat@gmail.com>
2021-10-26 23:20:50 -0300

    Agrega funcion de validacion

--- src/components/CrearPartida.js
+++ src/components/CrearPartida.js
@@ -6,6 +6,16 @@ import ElegirNickname from './ElegirNickname';
 import Lobby from './Lobby';
 import './Opciones.css';
 
+function handleValidate(nickname, nombrePartida) {
+  console.log('saliendo por nickname', nickname.length, nickname);
+  if (nickname.length < 2) {
+    return [false, 'El nickname debe tener al menos dos caracteres'];
+  } if (nombrePartida.length < 2) {
+    return [false, 'El nombre de la partida debe tener al menos dos caracteres'];
+  }
+  return [true, ''];
+}
+
 async function sendGameData(endpoint, nickname, nombrePartida) {
   const requestOptions = {
     method: 'POST',
@@ -26,15 +36,11 @@ async function sendGameData(endpoint, nickname, nombrePartida) {
       }
       return data;
     })
-    .catch((error) => {
-      return Promise.reject(error);
-    });
-    return data;
+    .catch((error) => Promise.reject(error));
+  return data;
 }
 
-
 const CrearPartida = (props) => {
-  
   const [nombrePartida, setNombrePartida] = useState('');
   const [nickname, setNickname] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
@@ -45,73 +51,86 @@ const CrearPartida = (props) => {
   const [idHost, setIdHost] = useState(0);
   const { endpoint } = props;
 
-  useEffect(() => { 
+  useEffect(() => {
     let isMounted = true;
-    async function sendData(){
+    console.log('1', nickname, nombrePartida);
+    async function sendData() {
       setHasError(false);
-      sendGameData(endpoint, nickname, nombrePartida)
-      .then(async (response) =>{
-        if(isMounted){
-          setIdPartida(response?.idPartida);
-          setIdHost(response?.idHost);
-          setSubmited(false);
-          setCreada(true);
-        }
-      })
-      .catch((error) => {
-        setErrorMessage(error);
+      const validate = handleValidate(nickname, nombrePartida);
+      const validated = validate[0];
+      const validationMessage = validate[1];
+      console.log('validationMesagge:', validationMessage);
+      if (!validated) {
+        console.log('input invalido');
+        setErrorMessage(validationMessage);
         setHasError(true);
-      });
+        isMounted = false;
+      }
+      sendGameData(endpoint, nickname, nombrePartida)
+        .then(async (response) => {
+          if (isMounted) {
+            setIdPartida(response?.idPartida);
+            setIdHost(response?.idHost);
+            setSubmited(false);
+            setCreada(true);
+          }
+        })
+        .catch((error) => {
+          setErrorMessage(error);
+          setHasError(true);
+        });
     }
-    if(submited){
+    if (submited) {
       sendData();
     }
     return () => {
       isMounted = false;
     };
-
-  },[submited, endpoint, nickname, nombrePartida]);
+  }, [submited, endpoint, nickname, nombrePartida]);
 
   if (hasError) {
     return (
       <div>
         <p className="errorMessage">
-          {errorMessage} , por favor recargue la pagina..
+          {errorMessage}
+          {' '}
+          , por favor recargue la pagina..
         </p>
       </div>
     );
   }
-  if (creada){
-    return(
-      <Lobby 
-      idPartida={idPartida}
-      nombrePartida={nombrePartida}
-      idHost={idHost}
-      nicknameHost={nickname}
+  if (creada) {
+    return (
+      <Lobby
+        idPartida={idPartida}
+        nombrePartida={nombrePartida}
+        idHost={idHost}
+        nicknameHost={nickname}
       />
     );
   }
 
-    return (
-      <div className="inputBox">
-        <form onSubmit={ event =>{ 
-          event.preventDefault();
-          setSubmited(true);
-          }}>
-          <Stack direction="row" alignItems="center" spacing={2}>
-            <ElegirNombrePartida
-              setNombrePartida={setNombrePartida}
-            />
-            <ElegirNickname
-              setNickName={setNickname}
-            />
-            <Button type="submit" variant="contained">
-              Crear
-            </Button>
-          </Stack>
-        </form>
-      </div>
-    );
+  return (
+    <div className="inputBox">
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        setSubmited(true);
+      }}
+      >
+        <Stack direction="row" alignItems="center" spacing={2}>
+          <ElegirNombrePartida
+            setNombrePartida={setNombrePartida}
+          />
+          <ElegirNickname
+            setNickName={setNickname}
+          />
+          <Button type="submit" variant="contained">
+            Crear
+          </Button>
+        </Stack>
+      </form>
+    </div>
+  );
 };
 
 export default CrearPartida;
