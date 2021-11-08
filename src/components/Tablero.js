3f728787cfa3d4f80c44cf946f1a053dedc14b76
Juan Domandl <jdomandl@Juans-MacBook-Pro.local>
2021-11-08 03:35:45 -0300

    tablero con recintos

--- src/components/Tablero.js
+++ src/components/Tablero.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { useEffect, useRef, useState } from 'react';
 import useMouse from '@react-hook/mouse-position'
+import spriteCochera from '../sprites/spriteCochera.png'; 
+import spriteVestibulo from '../sprites/spriteVestibulo.png';
+import spriteBodega from '../sprites/spriteBodega.png';
+import spriteAlcoba from '../sprites/spriteAlcoba.png';
+import spriteMisterio from '../sprites/spriteMisterio.png';
+import spriteSalon from '../sprites/spriteSalon.png'; 
+import spriteBiblioteca from '../sprites/spriteBiblioteca.png';
+import spritePanteon from '../sprites/spritePanteon.png';
+import spriteLaboratorio from '../sprites/spriteLaboratorio.png';;
 
 const widthTablero = 1300;
 const heightTablero = 1300; 
@@ -25,19 +34,10 @@ const getPixelRatio = context => {
 function dibujarCasilleroVacio(ctx, i, j){
   ctx.rect(centerX + j*casilleroSize, centerY + i*casilleroSize, 
                 casilleroSize, casilleroSize);
-  ctx.strokeStyle = "white";
+  ctx.strokeStyle = 'white';
   ctx.stroke();
 }
 
-function dibujarCasilleroEntrada(ctx, dir, textAlign, i, j){
-  const margin = casilleroSize / 2;
-  ctx.textAlign = textAlign;
-
-  ctx.fillStyle = 'white'; 
-  ctx.fillText(dir,
-      centerX + j*casilleroSize + margin, 
-      centerY + i*casilleroSize + margin); 
-}
 
 function dibujarCasilleroClickeado(ctx, i, j){
   ctx.fillStyle = 'white';
@@ -50,7 +50,6 @@ function dibujarCasilleroDisponible(ctx, i, j){
   ctx.fillStyle = 'lightgray';
   ctx.fillRect(centerX + j*casilleroSize, centerY + i*casilleroSize, 
                 casilleroSize, casilleroSize);
-
 }
 
 function dibujarCasilleroOcupado(ctx, color, nickName, i, j){
@@ -65,14 +64,92 @@ function dibujarCasilleroOcupado(ctx, color, nickName, i, j){
   
   ctx.stroke();
   ctx.fill();
-  ctx.font = "30px Arial";
-  ctx.fillStyle = color;
-  const textMarginX = 80;
-  const textMarginY = -100;
-    
-  ctx.fillText(nickName,
-      centerX + j*casilleroSize + textMarginX, 
-      centerY + i*casilleroSize - textMarginY);
+  ctx.font = '30px Helvetica';
+  ctx.textBaseline = 'top';  
+  ctx.fillStyle = 'black';
+  const textMarginX = 60;
+  const textMarginY = -60;
+  const textX = centerX + j*casilleroSize + textMarginX;
+  const textY =  centerY + i*casilleroSize - textMarginY;
+  const width = ctx.measureText(nickName).width;
+
+  ctx.fillRect( textX, textY, width, 30);
+  ctx.fillStyle = color; 
+  ctx.fillText(nickName, textX, textY);
+
+}
+
+function dibujarRecintoCochera(ctx){
+  const img = new Image();
+  img.src = spriteCochera;
+  ctx.drawImage(img, centerX + 0*casilleroSize, 
+                centerY + 0*casilleroSize, 390, 390);
+
+}
+
+function dibujarRecintoVestibulo(ctx){
+  const img = new Image();
+  img.src = spriteVestibulo;
+  ctx.drawImage(img, centerX + 0*casilleroSize, 
+                centerY + 7*casilleroSize, 390, 390);
+
+}
+
+
+function dibujarRecintoBodega(ctx){
+  const img = new Image();
+  img.src = spriteBodega;
+  ctx.drawImage(img, centerX + 0*casilleroSize, 
+                centerY + 14*casilleroSize, 390, 390);
+
+}
+
+
+function dibujarRecintoAlcoba(ctx){
+  const img = new Image();
+  img.src = spriteAlcoba;
+  ctx.drawImage(img, centerX + 7*casilleroSize, 
+                centerY + 0*casilleroSize, 390, 390);
+
+}
+
+function dibujarRecintoMisterio(ctx){
+  const img = new Image();
+  img.src = spriteMisterio;
+  ctx.drawImage(img, centerX + 7*casilleroSize, 
+                centerY + 7*casilleroSize, 390, 390);
+
+}
+
+function dibujarRecintoSalon(ctx){
+  const img = new Image();
+  img.src = spriteSalon;
+  ctx.drawImage(img, centerX + 7*casilleroSize, 
+                centerY + 14*casilleroSize, 390, 390);
+
+}
+
+function dibujarRecintoBiblioteca(ctx){
+  const img = new Image();
+  img.src = spriteBiblioteca;
+  ctx.drawImage(img, centerX + 14*casilleroSize, 
+                centerY + 0*casilleroSize, 390, 390);
+
+}
+
+function dibujarRecintoPanteon(ctx){
+  const img = new Image();
+  img.src = spritePanteon;
+  ctx.drawImage(img, centerX + 14*casilleroSize,
+                centerY + 7*casilleroSize, 390, 390);
+
+}
+
+function dibujarRecintoLaboratorio(ctx){
+  const img = new Image();
+  img.src = spriteLaboratorio;
+  ctx.drawImage(img, centerX + 14*casilleroSize, 
+                centerY + 14*casilleroSize, 390, 390);
 
 }
 
@@ -100,8 +177,21 @@ function Tablero(props) {
     canvas.height = height * ratio;
         
     ctx.rect(centerX, centerY, widthTablero, heightTablero);
-    ctx.fillStyle = "black";
+    ctx.fillStyle = 'black';
     ctx.fill();
+     
+    dibujarRecintoCochera(ctx);
+    dibujarRecintoVestibulo(ctx);
+    dibujarRecintoBodega(ctx);
+
+    dibujarRecintoAlcoba(ctx);
+    dibujarRecintoMisterio(ctx);
+    dibujarRecintoSalon(ctx);
+    
+    dibujarRecintoBiblioteca(ctx);
+    dibujarRecintoPanteon(ctx);
+    dibujarRecintoLaboratorio(ctx);
+
     for (let i = 0; i < cantidadCasilleros; i++){
       for (let j = 0; j < cantidadCasilleros; j++){
         if ( [6, 13].includes(i) || [6, 13].includes(j)){
@@ -117,33 +207,17 @@ function Tablero(props) {
           if (iMouse == i && jMouse == j){
             dibujarCasilleroClickeado(ctx, i, j);
           }
-          dibujarCasilleroOcupado(ctx, "green", "Yo", pos[0], pos[1]);
+          dibujarCasilleroOcupado(ctx, 'white', 'Yo', pos[0], pos[1]);
         } 
       }
     }
-    dibujarCasilleroEntrada(ctx, "◄", "end", 2, 6);
-    dibujarCasilleroEntrada(ctx, "◄", "end", 15, 6);
-    dibujarCasilleroEntrada(ctx, "◄", "end", 10, 6);
-    dibujarCasilleroEntrada(ctx, "►", "left", 4, 13);
-    dibujarCasilleroEntrada(ctx, "►", "left", 10, 13);
-    dibujarCasilleroEntrada(ctx, "►", "left", 16, 13);
-    dibujarCasilleroEntrada(ctx, "▲", "center", 13, 3);
-    dibujarCasilleroEntrada(ctx, "▼", "center", 13, 10);
-    dibujarCasilleroEntrada(ctx, "▲ ", "center", 13, 16);
-    dibujarCasilleroEntrada(ctx, "▼", "center", 6, 4);
-    dibujarCasilleroEntrada(ctx, "▲", "center", 6, 10);
-    dibujarCasilleroEntrada(ctx, "▼", "center", 6, 15);
     
-    
-    dibujarCasilleroOcupado(ctx, "green", "George", 6, 0);
-    //dibujarCasilleroOcupado(ctx, "red", "Ringo", 9, 6);
-    dibujarCasilleroOcupado(ctx, "pink", "Paul", 9, 13);
-    //dibujarCasilleroOcupado(ctx, "blue", "John", 13, 2);
+    dibujarCasilleroOcupado(ctx, 'green', 'GEROGE', 6, 0);
+    dibujarCasilleroOcupado(ctx, 'red', 'RINGO', 9, 6);
+    dibujarCasilleroOcupado(ctx, 'pink', 'PAUL', 9, 13);
+    dibujarCasilleroOcupado(ctx, 'blue', 'JOHN', 13, 2);
     
 
-    ctx.font = "30px Arial";
-    ctx.fillStyle = 'white';
-    ctx.fillText("Misterio", centerX + 600, centerY + 650);
   });
 
   return (
