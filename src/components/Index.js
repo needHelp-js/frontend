19adf01df6200a7524735d620968f5dbb3343a33
Matiata <matizmurat@gmail.com>
2021-11-17 20:33:37 -0300

    borra mas lineas viejas

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ListarPartidas from './ListarPartidas';
 import Lobby from './Lobby/Lobby';
 import Main from './Main';
 import CrearPartida from './CrearPartida/CrearPartida';
 import Partida from './Partida';
-import ElegirMonstruo from './ElegirMonstruo';
-import ElegirVictima from './ElegirVictima';
 import {
   URL_HOME,
   URL_LISTAR_PARTIDAS,
   URL_CREAR_PARTIDA,
   URL_LOBBY,
   URL_PARTIDA,
-  URL_VICTIMA,
-  URL_MONSTRUO,
 } from '../routes';
 
 const URL_SERVER = process.env.REACT_APP_URL_SERVER;
