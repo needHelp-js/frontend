f6cfcfd15f0f2fbbc8f151a869e0e903f3f71c35
Matiata <matizmurat@gmail.com>
2021-11-20 00:01:23 -0300

    Agrega SocketSingleton, test y arregla donde corresponda

---
+++ src/__tests__/SocketSingleton.test.js
@@ -0,0 +1,32 @@
+import WS from 'jest-websocket-mock';
+import SocketSingleton from '../components/connectionSocket';
+import SingletonAlreadyInitialized from '../components/errorSocket';
+
+describe('Test del socket', () => {
+  it('Crea una instancia correctamente', () => {
+    const idPartida = 1;
+    const idPlayer = 1;
+    const urlWebsocket = process.env.REACT_APP_URL_WS.concat('/', idPartida, '/ws/', idPlayer);
+    SocketSingleton.init(new WS(urlWebsocket));
+    const socket = SocketSingleton.getInstance();
+    expect(socket).not.toBeNull();
+  });
+  it('Es singleton', () => {
+    const idPartida = 2;
+    const idPlayer = 2;
+    const urlWebsocket = process.env.REACT_APP_URL_WS.concat('/', idPartida, '/ws/', idPlayer);
+    expect(() => SocketSingleton.init(new WS(urlWebsocket))).toThrow(SingletonAlreadyInitialized);
+  });
+  it('Devuelve la misma instancia', () => {
+    const instance1 = SocketSingleton.getInstance();
+    const instance2 = SocketSingleton.getInstance();
+    expect(instance2).toBe(instance1);
+  });
+  it('Destruye la instancia con el metodo correspondiente', () => {
+    const instance = SocketSingleton.getInstance();
+    expect(instance).not.toBe(null);
+    SocketSingleton.destroy();
+    const instance2 = SocketSingleton.getInstance();
+    expect(instance2).toBe(null);
+  });
+});
--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,8 +1,9 @@
 import { Button } from '@mui/material';
-import React, { useEffect, createRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { URL_PARTIDA } from '../routes';
 import ListarJugadores from './ListarJugadores';
+import SocketSingleton from './connectionSocket';
 import './Lobby.css';
 
 async function requestStart(idPartida, idPlayer) {
@@ -32,17 +33,17 @@ function Lobby(props) {
   const [started, setStarted] = useState(false);
   const wsPrefix = process.env.REACT_APP_URL_WS;
   const socketURL = wsPrefix.concat('/', idPartida, '/ws/', idPlayer);
-  const playerSocket = createRef();
 
   useEffect(() => {
-    playerSocket.current = new WebSocket(socketURL);
+    SocketSingleton.init(new WebSocket(socketURL));
+    console.log('ws singleton es:', SocketSingleton.getInstance());
     setPlayerJoined(true);
     let isMounted = true;
 
-    playerSocket.current.onopen = () => {
+    SocketSingleton.getInstance().onopen = () => {
       console.log('socket created:', idPartida, idPlayer);
     };
-    playerSocket.current.onmessage = (event) => {
+    SocketSingleton.getInstance().onmessage = (event) => {
       const message = JSON.parse(event.data);
       if (message.type === 'PLAYER_JOINED_EVENT' && isMounted) {
         setPlayerJoined(true);
@@ -73,6 +74,10 @@ function Lobby(props) {
     return (
       <Redirect to={{
         pathname: URL_PARTIDA,
+        state: {
+          idPartida,
+          idPlayer,
+        },
       }}
       />
     );
