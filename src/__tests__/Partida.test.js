021aa08efdfd3fe61871b90ab62e363dfb1d1407
alvie97 <alvie97m@gmail.com>
2021-11-21 02:03:26 -0300

    arregla test

--- src/__tests__/Partida.test.js
+++ src/__tests__/Partida.test.js
@@ -5,6 +5,9 @@ import Partida from '../components/Partida';
 import SocketSingleton from '../components/connectionSocket';
 import { act } from 'react-dom/test-utils';
 import { monstersNames, roomsNames, victimsNames } from '../utils/constants';
+import { Redirect } from 'react-router-dom';
+import { URL_PARTIDA } from '../routes';
+import { Router } from 'react-router';
 
 const urlWebsocket = `${process.env.REACT_APP_URL_WS}/1/ws/1`;
 const wsServer = new WS(urlWebsocket);
@@ -23,32 +26,41 @@ afterEach(() => {
   SocketSingleton.destroy();
 });
 
-describe('Partida', () => {
-    it('renderiza el componente con las cartas', async () => {
-        act(() => {
-            render(<Partida />, container);
-        });
-        await wsServer.connected
+describe("Partida", () => {
+  it("renderiza el componente con las cartas", async () => {
+    act(() => {
+      render(
+        <Partida location={{ state: { idPartida: 1, idPlayer: 1 } }} />,
+        container
+      );
+    });
+    await wsServer.connected;
 
-        const cardNames = [victimsNames.AMA_DE_LLAVES, monstersNames.DRACULA, roomsNames.ALCOBA];
+    const cardNames = [
+      victimsNames.AMA_DE_LLAVES,
+      monstersNames.DRACULA,
+      roomsNames.ALCOBA,
+    ];
 
-        act(() => {
-            wsServer.send(JSON.stringify({
-                type: 'DEAL_CARDS_EVENT',
-                payload: cardNames
-            }));
-        });
+    act(() => {
+      wsServer.send(
+        JSON.stringify({
+          type: "DEAL_CARDS_EVENT",
+          payload: cardNames,
+        })
+      );
+    });
 
     const cardElems = container.firstChild.firstChild.children;
 
     let idx = 0;
     for (const cardElem of cardElems) {
-        let imageElem = cardElem.firstChild;
-        let elem = cardNames[idx];
+      let imageElem = cardElem.firstChild;
+      let elem = cardNames[idx];
 
-        expect(imageElem.alt).toBe(elem);
-        idx++;
+      expect(imageElem.alt).toBe(elem);
+      idx++;
     }
-    });
+  });
 });
 
