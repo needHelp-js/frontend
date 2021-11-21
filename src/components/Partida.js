bfa442be78c9442ec323bfef290260cf115af47d
Ale478 <alemusicmolin@gmail.com>
2021-11-21 16:56:18 -0300

    Agrega mostrar jugadores

--- src/components/Partida.js
+++ src/components/Partida.js
@@ -1,8 +1,22 @@
 import React from "react";
+import MostrarJugadores from "./MostrarJugadores";
 
 function Partida(){
     return(
-        <h2>Bienvenido a la Partida</h2>
+        <div>
+            <h2>Bienvenido a la Partida</h2>
+        
+            <h2>
+                {nombrePartida}
+            </h2>
+            <h4>Jugadores en la partida:</h4>
+            <MostrarJugadores
+                playerJoined={playerJoined}
+                setPlayerJoined={setPlayerJoined}
+                idPartida={idPartida}
+            />
+        </div>
+        
     )
 }
 
