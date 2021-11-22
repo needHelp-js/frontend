ae28409eebba275e49dba527154d5e835ae991d7
Matiata <matizmurat@gmail.com>
2021-11-22 01:53:48 -0300

    Agrega uso de componente Card

--- src/components/Sospechar/ElegirMonstruo.js
+++ src/components/Sospechar/ElegirMonstruo.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Stack from '@mui/material/Stack';
-import dracula from '../../Misterio_cartas/dracula.png';
-import fantasma from '../../Misterio_cartas/fantasma.png';
-import frankenstein from '../../Misterio_cartas/frankenstein.png';
-import hombreLobo from '../../Misterio_cartas/hombre_lobo.png';
-import jekyllHyde from '../../Misterio_cartas/Jekyll_hyde.png';
-import momia from '../../Misterio_cartas/momia.png';
+import Card from '../Carta'
+import { monstersNames } from '../../utils/constants';
 import '../Partida.css';
 
 function ElegirMonstruo(props) {
@@ -13,6 +9,7 @@ function ElegirMonstruo(props) {
   const [newSelected, setNewSelected] = useState('');
 
   function handleClick(id) {
+    console.log('click en', id)
     setNewSelected(id);
     const img = document.getElementById(id);
     img.className = 'selectedCard';
@@ -27,50 +24,23 @@ function ElegirMonstruo(props) {
     }
   }, [newSelected]);
 
+  let monstersCards = [];
+  for(var key in monstersNames){
+    if (!monstersNames.hasOwnProperty(key)) continue;
+    const id = monstersNames[key];
+      monstersCards.push( 
+        <Card
+          id={id}
+          key={id}
+          cardName={id}
+          onClick={() => handleClick(id)}
+        />
+    )
+  }
+
   return (
     <Stack direction="row" alignItems="center" spacing={2}>
-      <img
-        id="Drácula"
-        className="optionCard"
-        src={dracula}
-        alt="Drácula"
-        onClick={() => { handleClick('Drácula'); }}
-      />
-      <img
-        id="Fantasma"
-        className="optionCard"
-        src={fantasma}
-        alt="Fantasma"
-        onClick={() => { handleClick('Fantasma'); }}
-      />
-      <img
-        id="Frankenstein"
-        className="optionCard"
-        src={frankenstein}
-        alt="Frankenstein"
-        onClick={() => { handleClick('Frankenstein'); }}
-      />
-      <img
-        id="Momia"
-        className="optionCard"
-        src={momia}
-        alt="Momia"
-        onClick={() => { handleClick('Momia'); }}
-      />
-      <img
-        id="Hombre Lobo"
-        className="optionCard"
-        src={hombreLobo}
-        alt="Hombre Lobo"
-        onClick={() => { handleClick('Hombre Lobo'); }}
-      />
-      <img
-        id="Dr. Jekyll Mr Hyde"
-        className="optionCard"
-        src={jekyllHyde}
-        alt="Dr. Jekyll Mr Hyde"
-        onClick={() => { handleClick('Dr. Jekyll Mr Hyde'); }}
-      />
+      {monstersCards}
     </Stack>
   );
 }
--- src/components/Sospechar/ElegirVictima.js
+++ src/components/Sospechar/ElegirVictima.js
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Stack from '@mui/material/Stack';
+import Card from '../Carta'
+import { victimsNames } from '../../utils/constants';
 import '../Partida.css';
-import amaDeLlaves from '../../Misterio_cartas/ama_de_llaves.png';
-import doncella from '../../Misterio_cartas/doncella.png';
-import jardinero from '../../Misterio_cartas/jardinero.png';
-import mayordomo from '../../Misterio_cartas/mayordomo.png';
-import conde from '../../Misterio_cartas/conde.png';
-import condesa from '../../Misterio_cartas/condesa.png';
 
 function Elegirvictima(props) {
   const { victima, setVictima } = props;
   const [newSelected, setNewSelected] = useState('');
 
   function handleClick(id) {
+    console.log('click en', id)
     setNewSelected(id);
     const img = document.getElementById(id);
     img.className = 'selectedCard';
@@ -27,50 +24,23 @@ function Elegirvictima(props) {
     }
   }, [newSelected]);
 
+  let victimsCards = [];
+  for(var key in victimsNames){
+    if (!victimsNames.hasOwnProperty(key)) continue;
+    const id = victimsNames[key]; 
+      victimsCards.push( 
+        <Card
+          id={id}
+          key={id}
+          cardName={id}
+          onClick={() => handleClick(id)}
+        />
+      );
+  } 
+
   return (
     <Stack direction="row" alignItems="center" spacing={2}>
-      <img
-        id="Ama de llaves"
-        className="optionCard"
-        src={amaDeLlaves}
-        alt="Ama de llaves"
-        onClick={() => { handleClick('Ama de llaves'); }}
-      />
-      <img
-        id="Doncella"
-        className="optionCard"
-        src={doncella}
-        alt="Doncella"
-        onClick={() => { handleClick('Doncella'); }}
-      />
-      <img
-        id="Jardinero"
-        className="optionCard"
-        src={jardinero}
-        alt="Jardinero"
-        onClick={() => { handleClick('Jardinero'); }}
-      />
-      <img
-        id="Mayordomo"
-        className="optionCard"
-        src={mayordomo}
-        alt="Mayordomo"
-        onClick={() => { handleClick('Mayordomo'); }}
-      />
-      <img
-        id="Conde"
-        className="optionCard"
-        src={conde}
-        alt="Conde"
-        onClick={() => { handleClick('Conde'); }}
-      />
-      <img
-        id="Condesa"
-        className="optionCard"
-        src={condesa}
-        alt="Condesa"
-        onClick={() => { handleClick('Condesa'); }}
-      />
+      {victimsCards}
     </Stack>
   );
 }
