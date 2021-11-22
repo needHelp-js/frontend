d8242c783906238e64987318394211403968c346
Juan Domandl <jdomandl@Juans-MacBook-Pro.local>
2021-11-22 20:57:22 -0300

    Test finalizado

--- src/__tests__/Tablero.test.js
+++ src/__tests__/Tablero.test.js
@@ -7,9 +7,15 @@ import "jest-canvas-mock";
 configure({ adapter: new Adapter() });
 
 describe("Tablero", () => {
-  it("should render my component", () => {
+  it("1. Renderizar el tablero.", () => {
     const component = shallow(<Tablero />);
     expect(component.getElements()).toMatchSnapshot();
+  });
+  
+  it(" 2.El tablero debería 640x640 de resolución.", () => {
+    const component = shallow(<Tablero />);
+    expect(component.find("canvas").props().style.width).toEqual(640);
+    expect(component.find("canvas").props().style.height).toEqual(640);
   });
 
 });
